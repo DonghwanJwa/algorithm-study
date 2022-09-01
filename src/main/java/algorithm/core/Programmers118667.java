ce680fd8a0b9bf5fd0a99b321ae7f2dd0b82ba14
DONGHWAN <midnightinmyjourney@gmail.com>
2022-09-01 18:41:42 +0900

    Programmers 118667 설명 추가

--- src/main/java/algorithm/core/Programmers118667.java
+++ src/main/java/algorithm/core/Programmers118667.java
@@ -20,9 +20,9 @@ public class Programmers118667 {
 
     static int solution(int[] queue1, int[] queue2) {
         int count = 0;
-        long lq1 = 0l;
-        long lq2 = 0l;
-        Queue<Integer> q1 = new LinkedList<>();
+        long lq1 = 0l;  //queue1 의 총합
+        long lq2 = 0l;  //queue2 의 총합
+        Queue<Integer> q1 = new LinkedList<>(); //Array to Queue
         Queue<Integer> q2 = new LinkedList<>();
         for(int a : queue1) {
             lq1+=a;
@@ -33,18 +33,21 @@ public class Programmers118667 {
             q2.add(b);
         }
 
-        long lqt = lq1+lq2;
+        long lqt = lq1+lq2; //total
 
-        if(lqt%2==0) {    //짝수일때만 가능함
-            while(lq1!=lq2 && count < queue1.length*4){
-                if(lq1>lq2){
-                    int tmp = q1.poll();
-                    lq1-=tmp;
-                    lq2+=tmp;
-                    q2.add(tmp);
-                    count++;
+        if(lqt%2==0) {    //총 합이 짝수일때만 가능함
+            while(lq1!=lq2 && count < queue1.length*3){
+                //queue끼리의 총합이 같거나, 움직인횟수가 총 길이의 3배가 넘어가면 멈춤
+                // -> 3배가 넘어가면 멈추는 이유, 반복문으로 움직일때 길이의 3배만큼 움직이면 처음과 동일해지므로
+                //    더이상 계산할 필요가 없기 때문 = 불가능
+                if(lq1>lq2){    //합이큰 queue에서 합이 작은 queue로 값을 이동
+                    int tmp = q1.poll();    //1. 합이 큰곳에서 숫자 하나를 뺀다음(queue1)
+                    lq1-=tmp;               //2. queue1의 총합에서 빼고
+                    lq2+=tmp;               //3. queue2의 총합에서 뺀다.
+                    q2.add(tmp);            //4. 합이 작은곳에 숫자를 추가한다(queue2)
+                    count++;                //5. 움직임을 하나 추가한다.
                 }else{
-                    int tmp = q2.poll();
+                    int tmp = q2.poll();    //위와 동일함
                     lq2-=tmp;
                     lq1+=tmp;
                     q1.add(tmp);
@@ -52,12 +55,12 @@ public class Programmers118667 {
                 }
             }
 
-            if(count == queue1.length*4) {
+            if(count == queue1.length*3) {  //길이의 3배 넘어가면 불가능처리
                 return -1;
             }else{
                 return count;
             }
-        }else{
+        }else{  //홀수일때는 해당 로직 사용 불가
             return -1;
         }
     }
